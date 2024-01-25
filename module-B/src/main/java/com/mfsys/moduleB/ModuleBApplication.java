e4a8814b564ad25a1780330014bd6c10f5c3d098
Raja Nabeel <nabeelx64@icloud.com>
2024-01-25 14:42:46 +0500

    initial commit

---
+++ module-B/src/main/java/com/mfsys/moduleB/ModuleBApplication.java
@@ -0,0 +1,13 @@
+package com.mfsys.moduleB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleBApplication.class, args);
+	}
+
+}
